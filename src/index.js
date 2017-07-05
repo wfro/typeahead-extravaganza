d066f26a2cbd32c72ccc8d162d2b8c45cc771f04
Will Faurot <wfaurot@gmail.com>
2017-07-05 00:12:06 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -7,50 +7,44 @@
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-// const $root = document.getElementById('root');
-
-const $ul = document.querySelector('.items');
+const $root = document.getElementById('root');
 const $searchInput = document.querySelector('.search-input');
 
 // Global variable that will hold items when they're fetched
 // from the server
-let allItems = [];
+let state = {
+  items: [],
+  searchText: '',
+};
+
+function setState(updates) {
+  const nextState = {
+    ...state,
+    ...updates,
+  };
+  state = nextState;
+  render();
+}
 
 function init() {
-  // Add the event listener
   $searchInput.addEventListener('keyup', onSearchChange);
+
+  render();
 
   // Fetch the initial list of tasks
   window
     .fetch('http://localhost:3001/items')
     .then(resp => resp.json())
     .then(resp => {
-      allItems = resp;
-
       // Finally to the initial render of the items list
-      renderItems(allItems);
+      setState({ items: resp });
     });
 }
 
 // Runs every time a user types something new into the search input
 function onSearchChange(e) {
   // Get the search query we should filter by
-  const query = e.target.value;
-
-  const $lis = document.querySelectorAll('.item');
-
-  // Filter the list
-  Array.from($lis).forEach(li => {
-    const value = li.querySelector('h3').textContent;
-    if (
-      value &&
-      value.substr(0, query.length).toLowerCase() === query.toLowerCase()
-    ) {
-      li.style.display = '';
-    } else {
-      li.style.display = 'none';
-    }
-  });
+  setState({ searchText: e.target.value });
 }
 
 // Start the app
@@ -60,9 +54,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Helpers
 
-function renderItems(items) {
-  // Clear out the old list before we re-render
-  $ul.innerHTML = '';
+function render() {
+  renderItems();
+  renderCounter();
+}
+
+// <ul class="items">
+// </ul>
+function renderSearchInput() {
+  const input = elt('input', '', {
+    type: 'text',
+    placeholder: 'Search...',
+    class: 'search-input',
+    value: state.searchText,
+  });
+  input.addEventListener('keyup', onSearchChange);
+  $root.appendChild(input);
+}
+
+function renderItems() {
+  const $items = document.querySelector('.items');
+  let ul;
+  if ($items) {
+    $items.innerHTML = '';
+    ul = $items;
+  } else {
+    const $ul = elt('ul', '', { class: 'items' });
+    ul = $ul;
+  }
+
+  const query = state.searchText;
+  const filteredItems = state.items.filter(item => {
+    return (
+      item.name.substr(0, query.length).toLowerCase() === query.toLowerCase()
+    );
+  });
 
   // Interacting with the DOM (usually by selecting/removing/appending
   // an element) is expensive, so we ideally want to minimize how
@@ -71,7 +97,7 @@ function renderItems(items) {
   // has been built up.
   const documentFragment = document.createDocumentFragment();
 
-  items.forEach(item => {
+  filteredItems.forEach(item => {
     const li = elt('li', '', { class: 'item' });
     const header = elt('h3', item.name, { 'data-text': item.name });
     const body = elt('div', `Rating: ${item.rating}`);
@@ -80,7 +106,21 @@ function renderItems(items) {
     documentFragment.appendChild(li);
   });
 
-  $ul.appendChild(documentFragment);
+  $root.appendChild(documentFragment);
+}
+
+function renderCounter() {
+  const value = `Results: ${state.items.length}`;
+  const $results = document.querySelector('.results');
+  if ($results) {
+    $results.textContent = value;
+  } else {
+    const div = elt('div', `Results: ${state.items.length}`, {
+      class: 'results',
+    });
+
+    $searchInput.insertAdjacentHTML('afterend', div.outerHTML);
+  }
 }
 
 function elt(eltType, text, options = {}) {
