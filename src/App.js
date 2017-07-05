4284c5d587d24436aa0743f413de4a8483a76a55
Will Faurot <wfaurot@gmail.com>
2017-07-05 02:06:51 -0700

    finished-react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Steps: set up the initial markup
+// set up the event listener on the search input
+// make the call with the search query
+// Add a loading indicator
+// which method makes it easier to see the different stats for this particular element?
+// call setState with the response and render a list of items
+// add an empty state
+
+// Data vs. UI - break out data fetching/handling from presentation
+//   instacart examples
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.state = {
+      items: [],
+    };
+  }
+
+  onSearchChange(e) {
+    const query = e.target.value;
+
+    if (!query.length) {
+      this.setState({ items: [] });
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    window
+      .fetch(`http://localhost:3001/items?name_like=${query}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        this.setState({ items: resp, isLoading: false });
+      });
+  }
+
+  renderSearchResults() {
+    return this.state.items.map(item => (
+      <li className="item" key={item.id}>
+        <h3>{item.name}</h3>
+        <div>Rating: {item.rating}</div>
+      </li>
+    ));
+  }
+
+  renderResults() {
+    if (!this.state.items.length) {
+      if (this.state.isLoading) {
+        return <div>Loading...</div>;
+      }
+
+      return <div>No results!</div>;
+    }
+
+    return this.renderSearchResults();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="search-container">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search..."
+          onChange={this.onSearchChange}
+        />
+        <ul className="search-results">
+          {this.renderResults()}
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,114 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
 const $root = document.getElementById('root');
 
-// Global variable that will hold items when they're fetched
-// from the server
-let allItems = [];
+ReactDOM.render(<App />, $root);
 
-function init() {
-  initialRender();
-}
-
-function initialRender() {
-  // Render the container
-  const $container = elt('div', '', { class: 'search-container' });
-
-  // Render the search input
-  const $input = elt('input', '', {
-    class: 'search-input',
-    type: 'text',
-    placeholder: 'Search...',
-  });
-  $input.addEventListener('keyup', onSearchChange);
-
-  // Render results container
-  const $resultsContainer = elt('ul', '', { class: 'search-results' });
-
-  $container.appendChild($input);
-  $container.appendChild($resultsContainer);
-
-  // Finally render everything to the page
-  $root.appendChild($container);
-}
-
-function onSearchChange(e) {
-  // Get the search query we should filter by
-  const query = e.target.value;
-
-  // Don't make a request without a search term
-  if (!query.length) {
-    const $searchResults = document.querySelector('.search-results');
-    $searchResults.innerHTML = '';
-
-    return;
-  }
-
-  // Add a loading indicator while the request is in progress
-  const $searchResults = document.querySelector('.search-results');
-  // Clear out the old list before we re-render
-  $searchResults.innerHTML = 'Loading...';
-
-  window
-    .fetch(`http://localhost:3001/items?name_like=${query}`)
-    .then(resp => resp.json())
-    .then(resp => {
-      renderItems(resp);
-    });
-}
-
-// Start the app
-document.addEventListener('DOMContentLoaded', () => {
-  init();
-});
-
-// Helpers
-
-function renderItems(items) {
-  // Get the DOM node
-  const $searchResults = document.querySelector('.search-results');
-
-  // Clear out the old list before we re-render
-  $searchResults.innerHTML = '';
-
-  if (!items.length) {
-    $searchResults.innerHTML = 'No results!';
-  }
-
-  // Interacting with the DOM (usually by selecting/removing/appending
-  // an element) is expensive, so we ideally want to minimize how
-  // much we interact directly with the DOM. Using a document fragment
-  // ensures we only have to paint once, when the final list of elements
-  // has been built up.
-  const documentFragment = document.createDocumentFragment();
-
-  // Create a new DOM node for each item
-  items.forEach(item => {
-    const li = elt('li', '', { class: 'item' });
-    const header = elt('h3', item.name, { 'data-text': item.name });
-    const body = elt('div', `Rating: ${item.rating}`);
-    li.appendChild(header);
-    li.appendChild(body);
-    documentFragment.appendChild(li);
-  });
-
-  $searchResults.appendChild(documentFragment);
-}
-
-function elt(eltType, text, options = {}) {
-  const elt = document.createElement(eltType);
-  var textNode = document.createTextNode(text);
-  elt.appendChild(textNode);
-  Object.keys(options).forEach(k => {
-    const v = options[k];
-    elt.setAttribute(k, v);
-  });
-  return elt;
-}
+registerServiceWorker();
